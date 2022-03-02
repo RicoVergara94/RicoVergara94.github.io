1e3eab7b09f606f2b5693d5f4f75bc35dc34d89d
Oscar <oscar.vergara1994@gmail.com>
2022-03-02 17:45:34 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="Column1"></div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
